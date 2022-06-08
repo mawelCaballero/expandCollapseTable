94d614c4011e27024899fb4aa3b8a0bf7b9106b1
Manuel Caballero <manuelcaballero@gmail.com>
2022-06-09 00:29:00 +0200

    Adding popup and scroll for overflow

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
 
 $('input[type="checkbox"]').click(function(){
-    $(this).is(":checked") ? $('table table').show() : $('table table').hide();
+    $(this).is(":checked") ? $("table table").not('#popup-overlay').show() : $("table table").not('#popup-overlay').hide();
 });
 $('table table').hide();
 $("td > .fa").on("click",function(e){   
@@ -13,14 +13,23 @@ $("td > .fa").on("click",function(e){
 
  });
 
+
+
+
+
  $("td > .fa").hover(function(){
-     
      var innerTableContent =  $(this).closest('tr').next().find('td:first').html();
-     $(".popup-overlay").html('');
-     $(".popup-overlay").append(innerTableContent);
+     $(".popup-content").html('');
+     $(".popup-content").append(innerTableContent);
      $(".popup-overlay").find('table:first').css('display','block');
      $(".popup-overlay").show();
      
  
  });
+
+ $(".fa-close").on("click",function(e){   
+    $(".popup-overlay").hide();
+    });
+
+
 });
