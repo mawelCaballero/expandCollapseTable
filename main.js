ddfc45b99951744753bcb2e6d0b6ac247ee08e61
Manuel Caballero <manuel.garciacaballero@merkator.com>
2022-06-08 20:41:42 +0200

    integrating a new table

--- main.js
+++ main.js
@@ -12,4 +12,15 @@ $("td > .fa").on("click",function(e){
     $(this).closest('tr').next().find('table:first').toggle(); 
 
  });
+
+ $("td > .fa").hover(function(){
+     
+     var innerTableContent =  $(this).closest('tr').next().find('td:first').html();
+     $(".popup-overlay").html('');
+     $(".popup-overlay").append(innerTableContent);
+     $(".popup-overlay").find('table:first').css('display','block');
+     $(".popup-overlay").show();
+     
+ 
+ });
 });
