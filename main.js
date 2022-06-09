5eb600d96bc872faa0490227c33be66320f1758d
Manuel Caballero <manuel.garciacaballero@merkator.com>
2022-06-09 10:53:43 +0200

    Improving functionality, hover on collapse row is showing a popup

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
+$(document).ready(function () {
+  // Once page is loaded, hide the nested tables and show the first table
+  $("table table").hide();
 
-$(document).ready(function(){
+  // When checkbox is clicked, show all the nested tables, otherwise hide them
+  $('input[type="checkbox"]').click(function () {
+    $(this).is(":checked")
+      ? $("table table").not("#popup-overlay").show()
+      : $("table table").not("#popup-overlay").hide();
+  });
 
-$('input[type="checkbox"]').click(function(){
-    $(this).is(":checked") ? $("table table").not('#popup-overlay').show() : $("table table").not('#popup-overlay').hide();
-});
-$('table table').hide();
-$("td > .fa").on("click",function(e){   
+  // When a table header is clicked, show the nested table
+  $("td > .fa").on("click", function (e) {
+    e.preventDefault();
     $(this).toggleClass("fa-angle-down");
     $(this).toggleClass("fa-angle-up");
-    e.preventDefault();
-    $(this).closest('tr').next().find('table:first').toggle(); 
+    $(this).closest("tr").next().find("table:first").toggle();
+  });
 
- });
-
-
-
-
-
- $("td > .fa").hover(function(){
-     var innerTableContent =  $(this).closest('tr').next().find('td:first').html();
-     $(".popup-content").html('');
-     $(".popup-content").append(innerTableContent);
-     $(".popup-overlay").find('table:first').css('display','block');
-     $(".popup-overlay").show();
-     
- 
- });
-
- $(".fa-close").on("click",function(e){   
-    $(".popup-overlay").hide();
-    });
-
-
+  // When a expand or collapse icon is hovered, show a popup with the nested table information
+  // otherwise hide the popup
+  // If the nested table is visible, popup is not shown
+  $("td > .fa").hover(
+    function () {
+      if ($(this).hasClass("fa-angle-down")) {
+        var innerTableContent = $(this)
+          .closest("tr")
+          .next()
+          .find("td:first")
+          .html();
+        $(".popup-content").html("");
+        $(".popup-content").append(innerTableContent);
+        $(".popup-overlay").find("table:first").css("display", "block");
+        $(".popup-overlay").show();
+      }
+    },
+    function () {
+      console.log("Leave");
+      $(".popup-overlay").hide();
+    }
+  );
 });
